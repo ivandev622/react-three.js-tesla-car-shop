bc4c51a1cab79457a48e7abea65252e1c4f3ca53
ivandev622 <ivanpulkanov622@gmail.com>
2022-06-17 16:34:43 -0700

    cant be tsx at current moment

--- src/components/ThreeD.jsx
+++ src/components/ThreeD.jsx
@@ -3,26 +3,26 @@ import { ReactThreeFiber, Canvas, useFrame, extend, useThree } from 'react-three
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 extend({OrbitControls});
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      orbitControls: ReactThreeFiber.Object3DNode<OrbitControls, typeof OrbitControls>
-    }
-  }
-}
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       orbitControls: ReactThreeFiber.Object3DNode<OrbitControls, typeof OrbitControls>
+//     }
+//   }
+// }
 
 const Controls = () => {
   const {
     camera,
     gl
   } = useThree();
-  const controls = useRef<any>();
+  const controls = useRef();
   useFrame((state) => controls.current.update());
   return < orbitControls ref={controls} args={[camera, gl.domElement]} />; // was the capital of Orbit vs orbit
 }
 
-const Box = (props: any) => {
-  const ref = useRef<any>();
+const Box = (props) => {
+  const ref = useRef();
   useFrame(state => {
     ref.current.rotation.x += 0.01
     ref.current.rotation.y += 0.01
@@ -38,7 +38,6 @@ const Box = (props: any) => {
 }
 
 
-
 const ThreeD = () => {
 
   return (
@@ -49,6 +48,12 @@ const ThreeD = () => {
      <Box position={[1,1,0]}/>
      <axesHelper args={[5]}/>
      <Controls/>
+      <points>
+        <geometry>
+
+        </geometry>
+      </points>
+
     </Canvas>
   )
 }
