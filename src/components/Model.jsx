674477dd2756481685e196e6e7dffe9179e1aa43
ivandev622 <ivanpulkanov622@gmail.com>
2022-06-22 20:29:19 -0700

    console.loging the model to see if its building

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,7 +2,6 @@ import React, {lazy, Suspense} from 'react';
 import { useLoader } from '@react-three/fiber';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import {useFLTF} from '@react-three/drei';
 
 const Model = props =>{
 
@@ -10,7 +9,7 @@ const Model = props =>{
   const model = new useLoader(
     GLTFLoader, props.path
   )
-
+    console.log(model)
   return (
     <Suspense>
       <primitive object={model.scene} scale={'.50'} position={[0,0,0]}/>
