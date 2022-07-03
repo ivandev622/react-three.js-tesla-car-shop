8bb7c5c00b4ef77913fb06ec51f9c79c6f31ef71
ivandev622 <ivanpulkanov622@gmail.com>
2022-07-02 21:26:41 -0700

    camera toaget chanege implomented

--- src/components/CameraButtons.jsx
+++ src/components/CameraButtons.jsx
@@ -1,16 +1,43 @@
 import React from "react";
 import state from "../state";
+import Color from "./Color";
 
 const CameraButtons = ({}) => {
 
 
+  const sets = {
+    1: {
+      cameraPos: [7,7,7],
+      target: [4,0,0]
+    },
+    2: {
+      cameraPos: [-3,7,7],
+      target: [-4,0,0]
+    }
+  }
+
+  const handleClick = (num) => {
+    state.cameraPos.set(...sets[num].cameraPos)
+    state.target.set(...sets[num].target)
+    state.shouldUpdate = true;
+  }
+
   return (
 
     <div className="camera__buttons">
-      <button className=" camera__buttons camera__buttons--left">
+      <button className=" camera__buttons camera__buttons--left"
+        onClick={(e)=> {
+          handleClick(2)
+        }}
+
+      >
           {'<'}
       </button>
-      <button className=" camera__buttons camera__buttons--right">
+      <button className=" camera__buttons camera__buttons--right"
+        onClick={(e)=> {
+          handleClick(1)
+        }}
+      >
           {'>'}
       </button>
     </div>
