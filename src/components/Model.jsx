39d5f22bc9c74c24d29d2375122e0cf537e5b8a2
ivandev622 <ivanpulkanov622@gmail.com>
2022-07-08 19:17:47 -0700

    robot animations and clips

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,5 +1,5 @@
 import React, {lazy, Suspense} from 'react';
-import { useLoader } from '@react-three/fiber';
+import { useLoader, useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useBox } from '@react-three/cannon';
@@ -10,6 +10,21 @@ const Model = props =>{
   const model = useLoader(
     GLTFLoader, props.path
   )
+    console.log(props.path, model)
+
+    let mixer;
+    if (model.animations.length > 0) {
+      mixer = new THREE.AnimationMixer(model.scene);
+      model.animations.forEach((clip, index) => {
+        const action = mixer.clipAction(clip);
+        action.play();
+      })
+
+    }
+
+    useFrame((scene, delta)=> {
+      mixer?.update(delta)
+    });
 
   model.scene.traverse(child => {
     if (child.isMesh) {
